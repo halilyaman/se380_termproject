7f19a89ef9678d5affd5d921cde80b6c6882f095
Halil Yaman <40357552+halilyaman@users.noreply.github.com>
2019-04-28 15:48:46 +0300

    movie card designs

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,116 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
 
+  Widget buildMovieCard() {
+    return Container(
+      padding: EdgeInsets.only(top: 20),
+      child: ConstrainedBox(
+        constraints: BoxConstraints.expand(height: 500),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 6,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 6,
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints.expand(),
+                        child: Container(
+                          color: Colors.black,
+                          child: Image.asset(ImageAssets.myImage),
+                        ),
+                      )
+                    ),
+                    Expanded(
+                      flex: 5,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Expanded(
+                              child: ConstrainedBox(
+                                constraints: BoxConstraints.expand(),
+                                child: Container(
+                                  color: Colors.deepOrange,
+                                  child: Center(
+                                    child: Text("Movie Name"),
+                                  )
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: ConstrainedBox(
+                                constraints: BoxConstraints.expand(),
+                                child: Container(
+                                  color: Colors.yellowAccent,
+                                  child: Center(
+                                    child: Text("Movie Info"),
+                                  )
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 7,
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints.expand(),
+                        child: Container(
+                          color: Colors.cyanAccent,
+                          child: Center(
+                            child: Text("Stars"), // TODO: add ten evaluation stars
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints.expand(),
+                        child: Container(
+                          color: Colors.purpleAccent,
+                          child: Center(
+                            child: Text("9.2/10"), // TODO: add average score of movie
+                          )
+                        ),
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> movieCardList = List();
+
   @override
   Widget build(BuildContext context) {
 
-    return Column(
+    for(int i = 0; i < 3; i++)  {
+      movieCardList.add(buildMovieCard());
+    }
 
+
+    return SingleChildScrollView(
+      child: Column(
+        children: movieCardList
+      ),
     );
 
   }
